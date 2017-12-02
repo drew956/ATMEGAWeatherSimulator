c2a7de94774325074553907a66877806c323cd3f
Drew956 <logandrew95@yahoo.com>
2017-12-01 23:09:30 -0700

    final iteration

--- Chapter10_Pulse-Width-Modulation/pwm/pwm.c
+++ Chapter10_Pulse-Width-Modulation/pwm/pwm.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <avr/io.h>                  
-#include <util/delay.h>              
+#include <util/delay.h>   
+#include <string.h>           
 #include "USART.h"
 #include "lcd.h"
 
@@ -24,10 +25,17 @@ void delay_us(uint16_t count) {
 
   }
 }
+
+/* 
+    Reads a string up to a max length and returns it. 
+    Same as the one defined in the USART.c, except it doesn't 
+    echo the bytes it receives.
+*/
 void silentReadString(char myString[], uint8_t maxLength) {
   char response;
   uint8_t i;
   i = 0;
+  
   while (i < (maxLength - 1)) {                   /* prevent over-runs */
     response = receiveByte();
     //transmitByte(response);                                    /* echo */
@@ -41,8 +49,14 @@ void silentReadString(char myString[], uint8_t maxLength) {
   }
   myString[i] = 0;                          /* terminal NULL character */
 }
-
+/* 
+    Turns an ASCII sequence of numbers into their actual integer equivalent
+    Does not work with negative numbers.
+    That is something that will be implemented in the future.
+*/
 int convertToNumber(char p[]){
+    p = strrev(p);
+
     int i = 0;
     char thisChar = p[i];
     int sum = 0;
@@ -70,22 +84,34 @@ int main(void) {
         pb0 - enable bit for motor driver
     */
     DDRB   |= 0b00011111; //set pins to output
-    PORTB   = 0x00; //initialize (probably not necessary)
+    PORTB   = 0x00;       //initialize (probably not necessary)
 
     //turn on the motor
-    PORTB |= (1 << PB0); //digitalWrite (enableB, HIGH); //enable the motor    
+    PORTB |=  (1 << PB0); //digitalWrite (enableB, HIGH); //enable the motor    
     PORTB &= ~(1 << PB2); //        digitalWrite (MotorB1, LOW);
     PORTB |=  (1 << PB1); //        digitalWrite (MotorB2, HIGH);
     
-    int speed = 3;
-    int temperature = 85;//default, it's above "nice" and below "hot"
-    char speedChar[5];
-    char tempChar[5];
+    int  speed = 3;
+    int  temperature = 85;//default, it's above "nice" and below "hot"
+    char speedChar[5];   //used for reading in data and displaying it on the LCD
+    char tempChar[5];    //used for reading in data and displaying it on the LCD
     
     while (1) {
         
 
-        
+        /* 
+            Whenever we receive data
+                - try and stop the motor by going reverse for 200 milliseconds
+                   - it takes a long while for max speed to update to a slower speed
+                   - not sure how to fix this without adding in a longer delay based
+                     on the prior windspeed
+                - read in the new wind speed value
+                - read in the new temperature value
+                - update the LCD
+                  (because there is no need to update it until we get new data)
+                - set the motor in the right direction again.
+                
+        */
         if( bit_is_set(UCSR0A, RXC0) ){
             
             
@@ -99,17 +125,18 @@ int main(void) {
 
             //speed = readString();//getNumber();  
             //temperature = getNumber();   //this should wait until the data is ready
-            silentReadString(speedChar, 3);
-            transmitByte('y');
+            silentReadString(speedChar, 5);
+            printString(speedChar);
             
             
-            silentReadString(tempChar, 3);
-            transmitByte('y');
+            silentReadString(tempChar, 5);
+            printString(tempChar);
             
             //myPrintToLCD(speedChar);
             //myPrintToLCD(tempChar);
-            temperature = convertToNumber(tempChar);
             printWindSpeedAndTemperature(speedChar, tempChar);
+            temperature = convertToNumber(tempChar);
+            printWord(temperature);
             //set back to the right direction
             PORTB &=  ~(1 << PB2); //        digitalWrite (MotorB1, LOW);
             PORTB |=  (1 << PB1);  //        digitalWrite (MotorB2, HIGH);
@@ -129,33 +156,29 @@ int main(void) {
 /*
     software pwm
     maximum: 150
-    spectrum 150 = 1000 useconds
+    spectrum 150 = 1000 mseconds
              3   = 3 / 150 * 1000
                  = 1/50 * 1000
-                 20 us
-    high high causes motor to stop faster than if you turned it off
-    (due to it acting like a generator, magnetic field, etc)
+                 20 ms
+    description:
+        This function uses PWM to control the speed of the motor.
+        It does this by enabling and disabling the motor for a fraction of the time interval
+        The fraction is calculated based on the maximum wind speed of 150 miles per hour. 
+        This was based on the minimum wind speed of a Level 5 hurricane, which is the maximum 
+        danger level possible. (156 miles per hour)
+        
+    extra info:
+        The period of 500 milliseconds was determined by experimentation.
+        500 milliseconds was chosen as a good middle value for having 
+        the lower values be fast enough and the larger values slow enough.
+        Too low and the lower values would not move the motor very much.
 
-    1 rot / 10 seconds
-    0.1 rot / 1 second
-    
-    3 mph / x mph  * 1000 = 100
-    0.1 = 3 / x
-    x = 30 ??
-    doesn't work because we don't know how many RPMs it is at full voltage
-    
-    full ~= 5000 r / min
-    500 r/ 6 seconds
-    83 rotations / second
-    3 -> / 80
-    240
-    
-    240 -> 1 rot / second
 */
 void pwmMotor(int partsOn){
     int partsMax = 150;
-    int timeOn   = (500 * partsOn)/partsMax;
-    int timeOff  = 500 - timeOn;
+    int timeOn   = (500 * partsOn)/partsMax; //calculate how long it should be on or off
+    int timeOff  = 500 - timeOn;             //based on periods of 500 milliseconds
+                                            
     
     PORTB |= (1 << PB0); //digitalWrite (enableB, HIGH); //enable the motor    
     delay_ms(timeOn);
@@ -166,16 +189,25 @@ void pwmMotor(int partsOn){
     
 }
 
+/* 
+    Turn on the Green LED if Temperature is less than 90 degrees
+        and turn off the Red LED
+    Turn on the Red LED if the Temperature is Greater than or equal to 90 degrees
+        and turn off the Green LED
+*/
 void setTempLeds(int temperature){
     if(temperature < 90){
         PORTB |= 1 << PB3;    //turn on  green led
         PORTB &= ~(1 << PB4); //turn off red LED
     } else if(temperature >= 90){    
-        PORTB &= ~(1 << PB3); //turn off blue led
+        PORTB &= ~(1 << PB3); //turn off green led
         PORTB |= 1 << PB4;    //turn on  red LED
     }
 }
 
+/* 
+    Print a single string to the first line of the LCD
+*/
 void myPrintToLCD(char result[]){
         lcd_clrscr();
         lcd_gotoxy(0,0);
@@ -183,7 +215,11 @@ void myPrintToLCD(char result[]){
         _delay_ms(1000);
 
 }
-
+/* 
+    Print the current WindSpeed and Temperature to the LCD
+    on lines 1 and 2 respectively
+    It has a 1 second delay, although perhaps this should be removed.
+*/
 void printWindSpeedAndTemperature(char speed[], char temp[]){
     lcd_clrscr();
     lcd_gotoxy(0,0);
